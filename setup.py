3522da7cf3c0699519da4874fa9670c4461cd2ef
xiancg <chris.granados@xiancg.com>
2019-12-23 14:07:58 -0300

    add_template_rule added to init

--- setup.py
+++ setup.py
@@ -10,12 +10,12 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='cgx_naming',
-    version='1.1.1-beta',
+    version='1.1.2-beta',
     description='Naming conventions library',
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/xiancg/cgx_naming',
-    download_url='https://github.com/xiancg/cgx_naming/archive/v1.1.1-beta.tar.gz',
+    download_url='https://github.com/xiancg/cgx_naming/archive/v1.1.2-beta.tar.gz',
     author='Cesar Saez, Chris Granados- Xian',
     author_email='info@chrisgranados.com',
     classifiers=[
--- src/cgx_naming/__init__.py
+++ src/cgx_naming/__init__.py
@@ -2,6 +2,6 @@
 from __future__ import absolute_import, print_function
 
 from cgx_naming.naming import parse, solve, get_repo, save_session, load_session
-from cgx_naming.rules import add_rule, remove_rule, has_rule, reset_rules, get_active_rule, set_active_rule, get_rule, get_rules, save_rule, load_rule
+from cgx_naming.rules import add_rule, add_template_rule, remove_rule, has_rule, reset_rules, get_active_rule, set_active_rule, get_rule, get_rules, save_rule, load_rule
 from cgx_naming.tokens import add_token, add_token_number, remove_token, has_token, reset_tokens, get_token, get_tokens, save_token, load_token
 from cgx_naming.separators import add_separator, remove_separator, has_separator, reset_separators, get_separator, get_separators, save_separator, load_separator
