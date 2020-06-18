751a61345f0f80bcb8eba5ac8184cf7e3909185f
Chris Granados- Xian <info@chrisgranados.com>
2020-06-18 17:40:14 -0300

    Removed separator from init

--- src/vfxnaming/__init__.py
+++ src/vfxnaming/__init__.py
@@ -1,5 +1,4 @@
 # coding=utf-8
-from __future__ import absolute_import, print_function
 
 from vfxnaming.naming import parse, solve, get_repo, save_session, load_session  # noqa: F401
 from vfxnaming.rules import (  # noqa: F401
@@ -9,8 +8,4 @@ from vfxnaming.rules import (  # noqa: F401
 from vfxnaming.tokens import (  # noqa: F401
     add_token, add_token_number, remove_token, has_token, reset_tokens,
     get_token, get_tokens, save_token, load_token
-)
-from vfxnaming.separators import (  # noqa: F401
-    add_separator, remove_separator, has_separator, reset_separators,
-    get_separator, get_separators, save_separator, load_separator
 )
