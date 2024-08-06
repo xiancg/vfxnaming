7cf4ddffc9d15da27043cabb07080be160e542cc
Chris Granados- Xian <info@chrisgranados.com>
2024-08-05 23:59:29 -0300

    Fixing outdated tox

--- src/vfxnaming/__init__.py
+++ src/vfxnaming/__init__.py
@@ -1,11 +1,28 @@
-# coding=utf-8
-
 from vfxnaming.naming import parse, solve, get_repo, save_session, load_session  # noqa: F401
 from vfxnaming.rules import (  # noqa: F401
-    add_rule, remove_rule, has_rule, reset_rules, get_active_rule, set_active_rule,
-    get_rule, get_rules, save_rule, load_rule, Rule
+    add_rule,
+    remove_rule,
+    has_rule,
+    reset_rules,
+    get_active_rule,
+    set_active_rule,
+    get_rule,
+    get_rules,
+    save_rule,
+    load_rule,
+    Rule,
 )
 from vfxnaming.tokens import (  # noqa: F401
-    add_token, add_token_number, remove_token, has_token, reset_tokens,
-    get_token, get_tokens, save_token, load_token
+    add_token,
+    add_token_number,
+    remove_token,
+    has_token,
+    reset_tokens,
+    get_token,
+    get_tokens,
+    save_token,
+    load_token,
+    Token,
+    TokenNumber,
 )
+from vfxnaming.error import ParsingError, SolvingError, TokenError  # noqa: F401
