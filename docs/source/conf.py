870da79de865dc911d4a245bc4c896d1b09606fe
Chris Granados- Xian <info@chrisgranados.com>
2024-08-05 22:34:31 -0300

    Update docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,48 +12,28 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../..'))
+
+sys.path.insert(0, os.path.abspath("../.."))
 
 
 # -- Project information -----------------------------------------------------
 
-project = 'VFX Naming Conventions Framework'
-copyright = '2019, Chris Granados- Xian, Cesar Saez'
-author = 'Chris Granados- Xian'
+project = "VFX Naming Conventions Framework"
+author = "Chris Granados- Xian"
 
-# The full version, including alpha/beta/rc tags
-release = '1.2.3-beta'
-
+# The master toctree document.
+root_doc = "index"
 
 # -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
-extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.coverage'
-]
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.coverage"]
 
-master_doc = 'index'
-
-# Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
-
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
-
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 # -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
-html_theme = 'sphinx_rtd_theme'
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
